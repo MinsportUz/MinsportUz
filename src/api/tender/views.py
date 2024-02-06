a9f7f1c95b87c454bb873812ddc86befd4b9997d
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-02-06 10:47:29 +0500

    change code at tender/views.py

--- src/api/tender/views.py
+++ src/api/tender/views.py
@@ -53,13 +53,21 @@ class AdmTenderNoticesView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, ]
 
     def get_queryset(self):
-        accept_language = self.request.META.get('HTTP_ACCEPT_LANGUAGE')
-        activate(accept_language)
         queryset = self.queryset
         if hasattr(self.queryset.model, 'title'):
             queryset = self.queryset.exclude(title__exact='')
 
         return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        return Response(serializer.data)
 
 
 class TenderNoticesView(viewsets.ModelViewSet):
